cfc1aa11f0b7c812382beb6377df338ffd41d9b7
Siddharth961 <jainsiddharth.rattan1@gmail.com>
2025-06-23 01:02:26 +0530

    Initial commit upto ch4

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <p>Customersss Paaagesss</p>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <p>Invoicesss Paaagesss</p>
+  )
+}
+
+export default page
